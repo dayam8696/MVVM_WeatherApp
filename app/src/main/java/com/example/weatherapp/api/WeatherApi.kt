5496bdf839dc0731300e6442a280f05fd5bb4d71
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-03-09 13:05:25 +0530

    added temperature layout

--- app/src/main/java/com/example/weatherapp/api/WeatherApi.kt
+++ app/src/main/java/com/example/weatherapp/api/WeatherApi.kt
@@ -5,6 +5,7 @@ import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+// // https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=d1d76e6f74c25f60b602ac34a75cc0e5
 interface WeatherApi {
     @GET("data/2.5/weather")
     fun getCurrentWeather(
